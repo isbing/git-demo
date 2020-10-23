e2f6acb55257d87e7fcbb9e94b2e30edb2a98708
songbing <404863881@qq.com>
2020-10-23 10:59:49 +0800

    最后一次提交

--- src/main/java/com/isbing/App.java
+++ src/main/java/com/isbing/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println("111111");
+        System.out.println("123456");
         System.out.println( "Hello World!" );
     }
 }
---
+++ src/main/java/com/isbing/Test1.java
@@ -0,0 +1,8 @@
+package com.isbing;
+
+/**
+ * Created by song bing
+ * Created time 2020/10/22 16:27
+ */
+public class Test1 {
+}
